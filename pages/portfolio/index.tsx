46aa47659b1802ebb4be03398d45825b53ea2bd3
Dystian En <distian.enyus@gmail.com>
2023-02-27 15:41:04 +0700

    change position icon

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -12,9 +12,9 @@ const Portfolio = () => {
           <a key={index} href={it.href} target='_blank' rel='noreferrer' className='relative grid place-items-center rounded-lg bg-grey hover:shadow-2xl'>
             <img className='h-60 md:h-44 w-full rounded-lg' src={it.img} alt={'thumbnail'} />
             <div className='h-2/5 w-full flex items-end p-3 absolute bottom-0 rounded-b-lg bg-gradient-to-t from-black to-transparent'>
-              <div className='flex flex-row gap-2'>
-                <h1 className='text-white text-sm'>{it.title}</h1>
+              <div className='flex flex-row gap-1'>
                 <AiOutlineLink className='text-white'/>
+                <h1 className='text-white text-sm'>{it.title}</h1>
               </div>
             </div>
           </a>
