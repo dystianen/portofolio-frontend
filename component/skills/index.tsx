4203a391aa255d5657e521df3386bb990d0b37ff
Dystian En <distian.enyus@gmail.com>
2023-02-27 15:01:03 +0700

    chore: style skills

--- component/skills/index.tsx
+++ component/skills/index.tsx
@@ -2,6 +2,7 @@ import react, { useEffect } from 'react';
 import { ProgressBar } from '../progress-bar';
 import { Title } from '../title';
 import data from '../../json/about.json';
+import { IoMdCheckmarkCircleOutline } from 'react-icons/io'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -13,10 +14,12 @@ export const Skills = () => {
   return (
     <div>
       <Title>Skills</Title>
-      <div className='grid grid-cols-2 gap-x-5 md:gap-x-10 gap-y-5'>
-        {data.skills.map((it, index) => (
-          <div key={index} data-aos='fade-up'>
-            <ProgressBar title={it.title} percentage={it.percentage} />
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-x-5 md:gap-x-10 gap-y-5'>
+        {data.skill.map((it, index) => (
+          <div key={index} data-aos='fade-up' className='flex flex-row items-center gap-2'>
+            <IoMdCheckmarkCircleOutline className='text-xl text-darkblue'/>
+            <p className='text-xl text-darkgreen'>{it}</p>
+            {/* <ProgressBar title={it.title} percentage={it.percentage} /> */}
           </div>
         ))}
       </div>
