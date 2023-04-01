a39e497697917708c071c386e407f946435adddb
Dystian En <dystian.en@kelaspintar.co.id>
2023-04-02 01:29:09 +0700

    chore: animation menu

--- component/layout/index.tsx
+++ component/layout/index.tsx
@@ -29,7 +29,7 @@ const DefaultLayout = ({ children }: PropsWithChildren<unknown>): JSX.Element =>
     <div id='container' className='container-body'>
       <header className={`shadow p-5 bg-white z-50`}>
         <div>
-          <h1>DEY.</h1>
+          <h1 className={'logo'} onClick={() => router.push('/home')}>DEY.</h1>
           <div className={'hidden md:flex flex-row gap-10'}>
             <a className={`hover:scale-110 ${path === 'home' && 'scale-110 text-darkgreen'} hover:cursor-pointer hover:text-darkgreen duration-300`} onClick={() => onChangeMenu('/home')}>Home</a>
             <a className={`hover:scale-110 ${path === 'about' && 'scale-110 text-darkgreen'} hover:cursor-pointer hover:text-darkgreen duration-300`} onClick={() => onChangeMenu('/about')}>About</a>
@@ -42,13 +42,19 @@ const DefaultLayout = ({ children }: PropsWithChildren<unknown>): JSX.Element =>
             <Image src='/assets/github-mark-white.png' width={25} height={25} alt='github' />
           </a>
 
-          <button onClick={() => setIsShowNavbar(!isShowNavbar)} className={'flex flex-row md:hidden'}>
-            {isShowNavbar ? <MdClose className='text-4xl' /> : <MdMenu className='text-4xl' />}
+          <button onClick={() => setIsShowNavbar(true)} className={'flex flex-row md:hidden'}>
+            <MdMenu className='text-4xl' />
           </button>
         </div>
       </header>
 
-      <div className={`z-50 fixed flex flex-col ${isShowNavbar ? 'visible' : 'invisible'} md:hidden gap-10 ${isDarkMode ? 'bg-gray-700' : 'bg-white'} min-h-40 w-full mt-20 rounded-b-[20px] p-10 shadow-2xl`}>
+      <div className={`z-50 fixed flex flex-col ${isShowNavbar ? 'translate-x-0' : '-translate-x-[100rem]'} duration-300 md:hidden gap-10 ${isDarkMode ? 'bg-gray-700' : 'bg-white'} h-screen w-full rounded-b-[20px] px-10 py-5 shadow-2xl`}>
+        <div className={'flex flex-row justify-between items-center'}>
+        <h1 className={'logo'} onClick={() => router.push('/home')}>DEY.</h1>
+          <button onClick={() => setIsShowNavbar(false)} className={'flex flex-row md:hidden'}>
+            <MdClose className='text-4xl' />
+          </button>
+        </div>
         <a onClick={() => onChangeMenu('/home')}>Home</a>
         <a onClick={() => onChangeMenu('/about')}>About</a>
         <a onClick={() => onChangeMenu('/portfolio')}>Portfolio</a>
