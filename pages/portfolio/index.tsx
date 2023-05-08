85b8500992e9b5414d7181894b8eb0521feec030
Dystian En <distian.enyus@gmail.com>
2023-05-08 11:29:07 +0700

    fix: error

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -9,9 +9,9 @@ const Portfolio = (props: PortfolioType) => {
       <Title>Portfolio</Title>
 
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-8 w-full'>
-        {props.data.map((it, index) => (
+        {props.data?.map((it, index) => (
           <a key={index} href={it.href} target='_blank' rel='noreferrer' className='relative grid place-items-center rounded-lg bg-grey hover:shadow-2xl'>
-            <img className='h-60 md:h-48 w-full rounded-lg' src={it.img} alt={'thumbnail'} />
+            <img className='h-60 md:h-52 w-full rounded-lg' src={it.img} alt={'thumbnail'} />
             <div className='h-2/5 w-full flex items-end p-3 absolute bottom-0 rounded-b-lg bg-gradient-to-t from-black to-transparent'>
               <div className='flex flex-row gap-1'>
                 <AiOutlineLink className='text-white'/>
@@ -28,7 +28,7 @@ const Portfolio = (props: PortfolioType) => {
 export async function getServerSideProps() {
   return {
     props: {
-      data: portfolio
+      data: portfolio.data
     },
   }
 }
