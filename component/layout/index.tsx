792f5e6361949f225a0fe4c37ec1e549fe43dd8d
Dystian En <distian.enyus@gmail.com>
2023-05-09 10:41:51 +0700

    remove loading

--- component/layout/index.tsx
+++ component/layout/index.tsx
@@ -1,28 +1,11 @@
-import React, { PropsWithChildren, useState, useEffect } from 'react';
+import React, { PropsWithChildren } from 'react';
 import { Header } from "@/component/header/page";
 
 const DefaultLayout = ({children}: PropsWithChildren<unknown>): JSX.Element => {
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 500);
-  }, [typeof window !== "undefined" && window.location.pathname]);
-
   return (
     <div id='container' className='container-body'>
-      {loading ? (
-        <div className="loader-container">
-          <div className="spinner"></div>
-        </div>
-      ) : (
-        <>
-          <Header/>
-          <main>{children}</main>
-        </>
-      )}
+      <Header/>
+      <main>{children}</main>
     </div>
   );
 };
