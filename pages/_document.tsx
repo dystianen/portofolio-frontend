e07464d3c03b36c6d38794dd1ff4ca21634a9786
Dystian En <distian.enyus@gmail.com>
2023-05-09 10:37:29 +0700

    fix: slow rendered image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>DEY</title>
+        <link rel="shortcut icon" href="/assets/profile.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
