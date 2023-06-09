2bff0064d770be150fb72a36cd65405981226305
dystian_en <distian.enyus@gmail.com>
2023-06-09 13:48:05 +0700

    feat: disabled inspect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import DefaultLayout from "@/component/layout";
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const disableRightClick = (e) => {
+      e.preventDefault(); // Prevent the default context menu
+    };
+
+    const disableInspect = (e) => {
+      if (e.keyCode === 123) {
+        e.preventDefault(); // Prevent F12 key from opening the browser console
+      }
+
+      if (e.ctrlKey && e.shiftKey && e.code === 'KeyI') {
+        e.preventDefault(); // Prevent Ctrl + Shift + I from opening the browser console
+      }
+    };
+
+    // Add event listeners to the document
+    document.addEventListener('contextmenu', disableRightClick);
+    document.addEventListener('keydown', disableInspect);
+
+    return () => {
+      // Remove event listeners when the component is unmounted
+      document.removeEventListener('contextmenu', disableRightClick);
+      document.removeEventListener('keydown', disableInspect);
+    };
+  }, []);
+
   return <DefaultLayout>
     <Component {...pageProps} />
   </DefaultLayout>
