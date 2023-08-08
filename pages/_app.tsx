475ce636bc5fb43940241f2a1f6eee5ee781524a
Dystian En <dystian.en@kelaspintar.id>
2023-08-08 20:37:34 +0700

    coba gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,34 +3,38 @@ import type { AppProps } from 'next/app'
 import DefaultLayout from "@/component/layout";
 import { useEffect } from 'react';
 
+import dynamic from "next/dynamic";
+const GTM = dynamic(() => import('@/hooks/googleTagManager'), { ssr: false })
+
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    const disableRightClick = (e: any) => {
-      e.preventDefault(); // Prevent the default context menu
-    };
+  // useEffect(() => {
+  //   const disableRightClick = (e: any) => {
+  //     e.preventDefault(); // Prevent the default context menu
+  //   };
 
-    const disableInspect = (e: any) => {
-      if (e.keyCode === 123) {
-        e.preventDefault(); // Prevent F12 key from opening the browser console
-      }
+  //   const disableInspect = (e: any) => {
+  //     if (e.keyCode === 123) {
+  //       e.preventDefault(); // Prevent F12 key from opening the browser console
+  //     }
 
-      if (e.ctrlKey && e.shiftKey && e.code === 'KeyI') {
-        e.preventDefault(); // Prevent Ctrl + Shift + I from opening the browser console
-      }
-    };
+  //     if (e.ctrlKey && e.shiftKey && e.code === 'KeyI') {
+  //       e.preventDefault(); // Prevent Ctrl + Shift + I from opening the browser console
+  //     }
+  //   };
 
-    // Add event listeners to the document
-    document.addEventListener('contextmenu', disableRightClick);
-    document.addEventListener('keydown', disableInspect);
+  //   // Add event listeners to the document
+  //   document.addEventListener('contextmenu', disableRightClick);
+  //   document.addEventListener('keydown', disableInspect);
 
-    return () => {
-      // Remove event listeners when the component is unmounted
-      document.removeEventListener('contextmenu', disableRightClick);
-      document.removeEventListener('keydown', disableInspect);
-    };
-  }, []);
+  //   return () => {
+  //     // Remove event listeners when the component is unmounted
+  //     document.removeEventListener('contextmenu', disableRightClick);
+  //     document.removeEventListener('keydown', disableInspect);
+  //   };
+  // }, []);
 
   return <DefaultLayout>
+    <GTM />
     <Component {...pageProps} />
   </DefaultLayout>
 }
