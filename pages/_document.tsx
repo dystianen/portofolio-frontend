15c31040c0fe460c994806ecedfc9832b36cb0a9
Dystian En <distian.enyus@gmail.com>
2025-01-15 13:33:54 +0700

    fix: thumbnail shared

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,10 @@ export default function Document() {
         <link rel="shortcut icon" href="/assets/profile.png" />
         <title>Dystian En Yusgiantoro</title>
         <meta property="og:title" content={"Dystian En"} />
-        <meta property="og:image" content={"/assets/thumbnail_share.png"} />
+        <meta
+          property="og:image"
+          content={"https://dystianen.vercel.app/assets/thumbnail-share.png"}
+        />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1"
