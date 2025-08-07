9fa6c67a12e58685cd953c661294c88542781b25
Dystian En <distian.enyus@gmail.com>
2025-08-07 09:54:30 +0700

    update portfolio

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: "standalone",
+  productionBrowserSourceMaps: false, // Menghilangkan source map agar build lebih kecil
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.bagiwebsite.com",
+        pathname: "/assets/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
